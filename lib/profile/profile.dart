8261dbcd410bd517a7166ee14764803fe4086e39
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2021-09-05 18:55:43 +0500

    image picker added
    add image picker to select image from gallery

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 
 class ImageGetter extends StatefulWidget {
@@ -6,6 +9,22 @@ class ImageGetter extends StatefulWidget {
 }
 
 class _ImageGetterState extends State<ImageGetter> {
+  File? image;
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+      setState(() {
+        this.image = imageTemp;
+      });
+    } on PlatformException catch (e) {
+      print('Access Rejected: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,19 +33,42 @@ class _ImageGetterState extends State<ImageGetter> {
           //=======================================//
           SizedBox(height: 10),
           CircleAvatar(
-              radius: (40),
-              backgroundColor: Colors.transparent,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image.asset("assets/Soud.jpg"),
-              )),
+            radius: (40),
+            backgroundColor: Colors.transparent,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: image != null
+                  ? Image.file(image!)
+                  : Image.asset("assets/Soud.jpg"),
+            ),
+          ),
           SizedBox(height: 12),
           Text('Name Here', style: TextStyle(fontWeight: FontWeight.bold)),
           SizedBox(height: 5),
           Text('Front-End UI', style: TextStyle(color: Colors.grey)),
 
           TextButton(
-              onPressed: () {},
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext bc) {
+                      return Container(
+                        child: new Wrap(
+                          children: <Widget>[
+                            new ListTile(
+                                leading: new Icon(Icons.camera_alt),
+                                title: new Text('Camera'),
+                                onTap: () => {}),
+                            new ListTile(
+                              leading: new Icon(Icons.image),
+                              title: new Text('Gallery'),
+                              onTap: () => pickImage(),
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              },
               child: FittedBox(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
